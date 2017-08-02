a7b8c745076683be838407c495796fa8e264d04d
weijun8687 <812085814@qq.com>
2017-08-02 10:29:00 +0800

    Initial Commit

---
+++ Collection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Collection
+//
+//  Created by WJ on 2017/8/2.
+//  Copyright © 2017年 WJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Collection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Collection
+//
+//  Created by WJ on 2017/8/2.
+//  Copyright © 2017年 WJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
